4ba585cc2015e6647a4bf5795ccfbc5cde88f1a5
sunny sun <sunhaihuan@gmail.com>
2017-10-13 16:22:17 +0800

    change priorities from 5 to 16, and add one task to send message
    if you just want to tx message through NRF2401+ and do not want response, you can open PURE_TX_DEBUG to close rx channel and disable auto response and  close resend.

--- FREERTOS/USER/main.c
+++ FREERTOS/USER/main.c
@@ -5,7 +5,20 @@
 #include "led.h"
 #include "nrf24.h"
 
+//#define		PURE_TX_DEBUG			1
 
+#define nRF24_WAIT_TIMEOUT         (uint32_t)0x000FFFFF
+
+// Result of packet transmission
+typedef enum {
+	nRF24_TX_ERROR  = (uint8_t)0x00, // Unknown error
+	nRF24_TX_SUCCESS,                // Packet has been transmitted successfully
+	nRF24_TX_TIMEOUT,                // It was timeout during packet transmit
+	nRF24_TX_MAXRT                   // Transmit failed with maximum auto retransmit count
+} nRF24_TXResult;
+
+
+void NRF24TX_Task(void * pvParameters);
 void LED0_Task(void * pvParameters);
 void LED1_Task(void * pvParameters);
 
@@ -13,17 +26,81 @@ static void preSetupHardware(void);
 
 static void NRF_GPIO_Init(void);
 
+static nRF24_TXResult nRF24_TransmitPacket(uint8_t *pBuf, uint8_t length);
+
 int main(void)
 {
 	  preSetupHardware();
     LED_Init();
 		NRF_GPIO_Init();
 
-    xTaskCreate(LED0_Task, (const char *)"LED0", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
-    xTaskCreate(LED1_Task, (const char *)"LED1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 4, NULL);
+    xTaskCreate(LED0_Task, (const char *)"LED0", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(LED1_Task, (const char *)"LED1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 
+		xTaskCreate(NRF24TX_Task, (const char *)"NRF24TX_Task", 512, NULL, configMAX_PRIORITIES - 1, NULL);
+	
     vTaskStartScheduler();
 }
+
+void NRF24TX_Task(void * pvParameters){
+		
+  /*configure nrf24 transmit parameters*/	
+	static nRF24_TXResult tx_res;
+	static const uint8_t nRF24_ADDR[] = {'E','S','B'};
+	uint8_t payload_length = 10;
+	uint8_t nrf24_payload[10] = {'i',' ','l','o','v','e',' ','s','t','m'};
+	
+	nRF24_SetRFChannel(40);  
+	nRF24_SetDataRate(nRF24_DR_250kbps);
+	nRF24_SetCRCScheme(nRF24_CRC_2byte);
+	nRF24_SetAddrWidth(3);
+		
+	//configure TX PIPE
+	nRF24_SetAddr(nRF24_PIPETX, nRF24_ADDR); // program TX address
+	nRF24_SetAddr(nRF24_PIPE0, nRF24_ADDR);  // program address for pipe#0, must be same as TX (for Auto-ACK)
+		
+	nRF24_SetTXPower(nRF24_TXPWR_0dBm); // Set TX power (maximum)
+		
+	nRF24_SetAutoRetr(nRF24_ARD_2500us, 10); // Configure auto retransmit: 10 retransmissions with pause of 2500s in between
+		
+	nRF24_EnableAA(nRF24_PIPE0); // Enable Auto-ACK for pipe#0 (for ACK packets)
+	
+	#ifdef PURE_TX_DEBUG 
+		nRF24_SetAutoRetr(nRF24_ARD_2500us, 0);
+		nRF24_DisableAA(nRF24_PIPE0);
+	#endif
+		
+	nRF24_SetOperationalMode(nRF24_MODE_TX); // Set operational mode (PTX == transmitter)
+		
+	nRF24_ClearIRQFlags(); // Clear any pending IRQ flags
+		
+	nRF24_SetPowerMode(nRF24_PWR_UP); // Wake the transceiver
+
+	
+	for(;;){
+		
+		tx_res = nRF24_TransmitPacket(nrf24_payload, payload_length);
+		
+		switch (tx_res) {
+			case nRF24_TX_SUCCESS:
+				//UART_SendStr("OK");
+				break;
+			case nRF24_TX_TIMEOUT:
+				//UART_SendStr("TIMEOUT");
+				break;
+			case nRF24_TX_MAXRT:
+				//UART_SendStr("MAX RETRANSMIT");
+				break;
+			default:
+				//UART_SendStr("ERROR");
+				break;
+		}
+    	//UART_SendStr("\r\n");
+		vTaskDelay(5 / portTICK_PERIOD_MS);  //delay 5ms
+	}
+	
+}
+
 
 void LED0_Task(void * pvParameters)
 {
@@ -136,4 +213,67 @@ static void preSetupHardware(void){
 		/* Configure HCLK clock as SysTick clock source. */
 		SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
 		
+}
+
+
+// Function to transmit data packet
+// input:
+//   pBuf - pointer to the buffer with data to transmit
+//   length - length of the data buffer in bytes
+// return: one of nRF24_TX_xx values
+
+static nRF24_TXResult nRF24_TransmitPacket(uint8_t *pBuf, uint8_t length) {
+	volatile uint32_t wait = nRF24_WAIT_TIMEOUT;
+	uint8_t status;
+
+	// Deassert the CE pin (in case if it still high)
+	nRF24_CE_L();
+
+	// Transfer a data from the specified buffer to the TX FIFO
+	nRF24_WritePayload(pBuf, length);
+
+	// Start a transmission by asserting CE pin (must be held at least 10us)
+	nRF24_CE_H();
+
+	// Poll the transceiver status register until one of the following flags will be set:
+	//   TX_DS  - means the packet has been transmitted
+	//   MAX_RT - means the maximum number of TX retransmits happened
+	// note: this solution is far from perfect, better to use IRQ instead of polling the status
+	do {
+		status = nRF24_GetStatus();
+		if (status & (nRF24_FLAG_TX_DS | nRF24_FLAG_MAX_RT)) {
+			break;
+		}
+	} while (wait--);
+
+	// Deassert the CE pin (Standby-II --> Standby-I)
+	nRF24_CE_L();
+
+	if (!wait) {
+		// Timeout
+		return nRF24_TX_TIMEOUT;
+	}
+
+	// Check the flags in STATUS register
+	//UART_SendStr("[");
+	//UART_SendHex8(status);
+	//UART_SendStr("] ");
+
+	// Clear pending IRQ flags
+    nRF24_ClearIRQFlags();
+
+	if (status & nRF24_FLAG_MAX_RT) {
+		// Auto retransmit counter exceeds the programmed maximum limit (FIFO is not removed)
+		return nRF24_TX_MAXRT;
+	}
+
+	if (status & nRF24_FLAG_TX_DS) {
+		// Successful transmission
+		return nRF24_TX_SUCCESS;
+	}
+
+	// Some banana happens, a payload remains in the TX FIFO, flush it
+	nRF24_FlushTX();
+
+	return nRF24_TX_ERROR;
 }
