13065a9a338385e52c2821d1bba148ae9a6fcf39
sunny sun <sunhaihuan@gmail.com>
2017-10-16 16:31:50 +0800

    mark PURE_TX_DEBUG and verify tx ok when open auto-ack and resend

--- FREERTOS/USER/predefines.h
+++ FREERTOS/USER/predefines.h
@@ -1,9 +1,13 @@
 #ifndef		__PRE_DEFINES_H
 #define		__PRE_DEFINES_H
 
-#define		PURE_TX_DEBUG			1   //use debug mode to tx data or not
+/* when you debug NRF24 tx ability, you can open PURE_TX_DEBUG to close TX auto-ACK and resender ability
+ * when open TRANSMIT_MODE it will open TX Mode, when you mark TRANSMIT_MODE it will open RX Mode 
+ */
 
-//#define		TRANSMIT_MODE			1   //if TRANSMIT_MODE defined as 1 = tx mode, if not defined rx mode
+//#define		PURE_TX_DEBUG			1   //use debug mode to tx data or not
+
+#define		TRANSMIT_MODE			1   //if TRANSMIT_MODE defined as 1 = tx mode, if not defined rx mode
 
 #endif
 
